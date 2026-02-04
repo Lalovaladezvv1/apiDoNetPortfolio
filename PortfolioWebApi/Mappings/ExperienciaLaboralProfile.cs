aa5d193da727dca75dafbca3a646fa10cbbf41aa
Lalovaladezvv1 <lalo.valadezvv1@gmail.com>
2026-02-04 01:38:09 -0600

    fix AutoMapper Experiencia Laboral

--- PortfolioWebApi/Mappings/ExperienciaLaboralProfile.cs
+++ PortfolioWebApi/Mappings/ExperienciaLaboralProfile.cs
@@ -1,36 +1,16 @@
 using AutoMapper;
 using PortfolioWebApi.Models;
-using System;
-using System.Globalization;
-
 public class ExperienciaLaboralProfile : Profile
 {
     public ExperienciaLaboralProfile()
     {
-        CreateMap<ExperienciaLaboralCreateDto, ExperienciaLaboral>()
-            .ForMember(dest => dest.FechaInicio, opt => opt.MapFrom(src => ParseDate(src.FechaInicio)))
-            .ForMember(dest => dest.FechaFin, opt => opt.MapFrom(src => ParseDate(src.FechaFin)));
+        CreateMap<ExperienciaLaboralCreateDto, ExperienciaLaboral>();
 
         CreateMap<ExperienciaLaboralUpdateDto, ExperienciaLaboral>()
             .ForMember(dest => dest.Id, opt => opt.Ignore())
             .ForMember(dest => dest.FechaCreacion, opt => opt.Ignore())
-            .ForMember(dest => dest.Activo, opt => opt.Ignore())
-            .ForMember(dest => dest.FechaInicio, opt => opt.MapFrom(src => ParseDate(src.FechaInicio)))
-            .ForMember(dest => dest.FechaFin, opt => opt.MapFrom(src => ParseDate(src.FechaFin)));
+            .ForMember(dest => dest.Activo, opt => opt.Ignore());
 
         CreateMap<ExperienciaLaboral, ExperienciaLaboralReadDto>();
-    }
-
-    private static DateOnly? ParseDate(string? date)
-    {
-        if (string.IsNullOrWhiteSpace(date)) return null;
-
-        return DateOnly.TryParseExact(
-            date,
-            "yyyy-MM-dd", 
-            CultureInfo.InvariantCulture, 
-            DateTimeStyles.None, 
-            out var parsedDate
-        ) ? parsedDate : null;
     }
 }
